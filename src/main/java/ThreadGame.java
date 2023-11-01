2ee0d919aa7da2ed664db9835b5d22b4d29271fa
Анастасия <145212274+anastaishafilonova@users.noreply.github.com>
2023-11-01 21:06:57 +0300

    Add files via upload
    add second homework

---
+++ src/main/java/ThreadGame.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Logger;
+
+public class ThreadGame implements Runnable {
+  private static final Logger logger = Logger.getLogger(ThreadGame.class.getSimpleName());
+  private static final Object lock = new Object();
+  static Random random = new Random();
+  CopyOnWriteArrayList<Integer> array = new CopyOnWriteArrayList<>();
+  int points;
+  String name;
+  public ThreadGame(String name) {
+    this.name = name;
+  }
+  @Override
+  public void run() {
+    while (array.size() < 100) {
+      int x = random.nextInt(1, 100);
+      synchronized (lock) {
+        if (!array.contains(x)) {
+          array.add(x);
+          points += 1;
+        }
+      }
+        if (array.size() == 100) {
+          logger.info("Winner`s name is " + name + " " +  points);
+          return;
+        }
+    }
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    var t1 = new Thread(new ThreadGame("First"));
+    var t2 = new Thread(new ThreadGame("Second"));
+    t1.start();
+    t2.start();
+  }
+}
