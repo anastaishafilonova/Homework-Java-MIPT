4445d754f9e1e5158c22fb09d4bc87e08c39e543
Анастасия <145212274+anastaishafilonova@users.noreply.github.com>
2023-11-02 13:30:39 +0300

    Update ThreadGame.java

--- src/main/java/ThreadGame.java
+++ src/main/java/ThreadGame.java
@@ -5,26 +5,29 @@ import java.util.logging.Logger;
 public class ThreadGame implements Runnable {
   private static final Logger logger = Logger.getLogger(ThreadGame.class.getSimpleName());
   private static final Object lock = new Object();
-  static Random random = new Random();
-  CopyOnWriteArrayList<Integer> array = new CopyOnWriteArrayList<>();
-  int points;
-  String name;
+  private static final  Random random = new Random();
+  
+  private CopyOnWriteArrayList<Integer> array = new CopyOnWriteArrayList<>();
+  private int points;
+  private String name;
+  
   public ThreadGame(String name) {
     this.name = name;
   }
+  
   @Override
   public void run() {
     while (array.size() < 100) {
-      int x = random.nextInt(1, 100);
+      int x = random.nextInt(1, 101);
       synchronized (lock) {
         if (!array.contains(x)) {
           array.add(x);
           points += 1;
         }
       }
-        if (array.size() == 100) {
-          logger.info("Winner`s name is " + name + " " +  points);
-          return;
+      if (array.size() == 100) {
+        logger.info("Winner`s name is " + name + " " +  points);
+        return;
         }
     }
   }
